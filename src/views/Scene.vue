4e2f6ae4543b71090a22fbd3488c8ab8d7ffc807
肖思汗 <389652405@qq.com>
2021-11-05 23:06:13 +0800

    提交初始代码

--- src/views/Scene.vue
+++ src/views/Scene.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="scene">
-    <canvas />
+    <canvas ref="canvas" />
   </div>
 </template>
 
@@ -10,12 +10,18 @@ import * as THREE from "three";
 export default {
   name: "Scene",
   //渲染器
-  rederer: new THREE.WebGLRenderer({
-    canvas: document.createElement("canvas"),
-  }),
+  rederer: THREE.WebGLRenderer,
   // 场景
   scene: new THREE.Scene(),
 
+  camera: new THREE.PerspectiveCamera(50, 1, 1, 2000), //创建透视相机
+
+  mounted() {
+    // 渲染器
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.canvas as HTMLCanvasElement,
+    });
+  },
   props: {},
 
   components: {},
